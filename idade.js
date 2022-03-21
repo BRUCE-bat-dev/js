5b18db033e57749bf1389d57a2d2dc3d48ce8a37
bruce-69 <eduardcrow555@gmail.com>
2022-03-21 10:58:21 -0300

    projeto2.js

---
+++ idade.js
@@ -0,0 +1,25 @@
+// Defina uma variável para o valor do ano do nascimento;
+// Defina uma variável para o valor do ano atual;
+// Defina uma variável que calcula o valor final da idade da pessoa;
+// Exiba uma mensagem final dizendo a idade da pessoa e a mensagem "Você está na flor da idade".
+// const pronpt = require('prompt-sync')();
+
+
+// const idade =  +pronpt ('ano de nacimento ?')
+
+// const atual =  +pronpt('ano atual ?')
+
+// const menos = (idade  -  atual)
+
+
+// console.log(`a sua idade é ${menos}`)
+// let resposta = '';
+
+// while (resposta !== 's') {
+//     resposta = pronpt('Você quer sair do loop?');                }
+ const n = 0;
+
+while (n < 5) {
+    console.log(n);
+    n++ ;
+}
---
+++ inpar_ou_par.js
@@ -0,0 +1,21 @@
+// const prompt = require('prompt-sync')(); 
+// const num = +prompt ('Digite um numero: ');
+//   if (num % 2 === 0 ) 
+//   {  console.log(`O numero ${num} é par ` ) 
+// } else { console.log(`O numero ${num} é impar` ) }
+
+
+
+const prompt = require('prompt-sync')();
+
+const  joke = ['pedra1, papel2 ,tesoura3' ];
+
+const  pedra  = prompt('petra  papel ou tesoura ')
+
+
+const escolhau = pedra 
+
+
+
+console.log ('Math.random(3)')
+
